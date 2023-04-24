421d6d101ff95c248eec0d4a2a63b1faa84d825d
maciiv <48811369+maciiv@users.noreply.github.com>
2023-04-24 18:24:27 +1000

    remove unnecessary imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import rollupReplace from "@rollup/plugin-replace"
-import * as path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode, ssrBuild }) =>{
