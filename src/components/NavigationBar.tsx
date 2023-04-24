668397fa096d9f34cac25bd221f22542628e1bc7
maciiv <48811369+maciiv@users.noreply.github.com>
2023-04-24 14:21:59 +1000

    upload

---
+++ src/components/NavigationBar.tsx
@@ -0,0 +1,35 @@
+import { FunctionComponent } from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css"
+import Logo from "../assets/react.svg"
+
+const NavigationBar: FunctionComponent = function () {
+
+    return(
+        <>
+            <Navbar bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="/">
+                        <img
+                        alt=""
+                        src={Logo}
+                        width="30"
+                        height="30"
+                        className="d-inline-block align-top"
+                        />{' '}
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav>
+                            <Nav.Link href="/square">Square</Nav.Link>
+                            <Nav.Link href="/triangle">Triangle</Nav.Link>
+                            <Nav.Link href="/trapezoid">Trapezoid</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+        </Navbar>
+      </>
+    )
+}
+
+export default NavigationBar
---
+++ src/pages/Home.tsx
@@ -0,0 +1,20 @@
+import { FunctionComponent } from "react";
+import { Col, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+const Home: FunctionComponent = function () {
+    return(
+        <Row className="mt-5">
+            <Col lg="12" md="12" className="d-flex mb-5">
+                <h4 className="mx-auto">Please select the figure you want to calculate the area</h4>
+            </Col>
+            <Col lg="12" md="12" className="d-flex justify-content-evenly">
+                <Link to="/square" className="btn btn-primary btn-lg w-25">Square</Link>
+                <Link to="/triangle" className="btn btn-primary btn-lg w-25">Triangle</Link>
+                <Link to="/trapezoid" className="btn btn-primary btn-lg w-25">Trapezoid</Link>
+            </Col>
+        </Row>
+    )
+}
+
+export default Home
