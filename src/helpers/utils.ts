902f51b01f8150e4a893ca5f4f0cac457e2cca7a
maciiv <48811369+maciiv@users.noreply.github.com>
2023-05-04 16:06:31 +1000

    remove unnecessary code

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -1,5 +1,3 @@
-import { IInput } from "../components/InputGroupInput"
-
 export function convertToMm(x: number, unit: string) {
     switch (unit) {
         case "cm":
@@ -20,28 +18,4 @@ export function convertFromMmSquared(x: number, unit: string) {
         default:
             return x
     }
-}
-
-export function createTableDataInput(inputs: IInput[]) {
-    const td = document.createElement("td")
-    const ul = document.createElement("ul")
-    inputs.forEach(i => {
-        const li = document.createElement("li")
-        li.innerHTML = `${i.value} ${i.unit}`
-        ul.appendChild(li)
-    })
-    td.appendChild(ul)
-    return td
-}
-
-export function createTableDataResult(result: IInput) {
-    const td = document.createElement("td")
-    td.innerHTML = `${result.value} ${result.unit}`
-    return td
-}
-
-export function createTableDataText(text: string) {
-    const td = document.createElement("td")
-    td.innerHTML = text
-    return td
 }
